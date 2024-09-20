3838269eae672ecfcae55f3e5dbefbbb4ceb5139
Zvend <soerenganske@hotmail.de>
2024-09-20 20:23:40 +0200

    main init

---
+++ source/dllmain.cpp
@@ -0,0 +1,14 @@
+#include "version/version.h"
+
+
+BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD  fdwReason, LPVOID)
+{
+    if (fdwReason == DLL_PROCESS_ATTACH)
+    {
+        printf("PID = %d\n", GetCurrentProcessId());
+        setupWrappers();
+    }
+
+    return TRUE;
+}
+
